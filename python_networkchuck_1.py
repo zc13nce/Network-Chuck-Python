97d058ab76d3d79f13a1eb01e9e748b694c6e5da
zc13nce <95826978+zc13nce@users.noreply.github.com>
2022-06-18 10:23:50 -0500

    Create python_networkchuck_1.py

---
+++ python_networkchuck_1.py
@@ -0,0 +1,15 @@
+# Episode 1
+
+# print("Hello World!!!!!!!!!!!")
+# print('I am Iron Man')
+# print("No, I am Tony Stark")
+# print("No, I am Poppy")
+# We are practicing Python....IT'S AWESOME
+
+print("""I am Iron Man. 
+      No, I am Tony Stark.
+      No I am Poppy.""")
+
+print("I am Iron Man. \n" + "No, I am Tony Stark. \n" + "No, I am Poppy.")
+
+print("I am Poppy\n" * 100)
